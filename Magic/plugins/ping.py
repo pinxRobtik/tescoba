650f23b4eb3ee9d7cf9433af23f3cc8d48c69b81
amanqs <116822442+amanqs@users.noreply.github.com>
2023-09-16 03:26:01 +0700

    Update ping.py

--- Magic/plugins/ping.py
+++ Magic/plugins/ping.py
@@ -5,7 +5,7 @@ from Magic import *
 from Magic.plugins import *
 from config import *
 
-@ubot.on_message(filters.command(["ping"], ".") & filters.me)
+@ubot.on_message(filters.command("ping", prefix) & filters.me)
 async def pinx(client: Client, message: Message):
     mulai = datetime.now()
     berhenti = datetime.now()
