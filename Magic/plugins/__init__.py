bd8d0f645744683ba31e7478b347df07d151bb14
amanqs <116822442+amanqs@users.noreply.github.com>
2023-09-15 15:29:08 +0700

    Create __init__.py

---
+++ Magic/plugins/__init__.py
@@ -0,0 +1,12 @@
+from glob import glob
+from os.path import basename, dirname, isfile
+
+def loadPlugins():
+    jalur = glob(f"{dirname(__file__)}/*.py")
+    return sorted(
+        [
+            basename(f)[:-3]
+            for Magic in jalur
+            if isfile(f) and Magic.endswith(".py") and not Magic.endswith("__init__.py")
+        ]
+    )
