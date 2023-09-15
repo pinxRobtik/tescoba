a5d34a5d164e40257e781065977a2d2bdf0d0b06
amanqs <116822442+amanqs@users.noreply.github.com>
2023-09-15 15:36:32 +0700

    Create ping.py

---
+++ Magic/plugins/ping.py
@@ -0,0 +1,11 @@
+from pyrogram import Client
+from datetime import datetime
+from Magic import *
+from config import *
+
+@Client.on_message(filters.command("ping", prefix) & filters.me)
+async def pinx(client: Client, message: Message):
+    mulai = datetime.now()
+    berhenti = datetime.now()
+    durasi = (end - start).microseconds / 1000
+    await message.reply_text(f"**Sepong!**\n" f"`%sms`" % (durasi))
