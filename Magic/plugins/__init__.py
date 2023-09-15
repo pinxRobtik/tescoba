f5821c9d6ea1edd400f993b5d0ccadeaafafc5e9
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-09-15 21:00:13 +0700

    update __init__.py

--- Magic/plugins/__init__.py
+++ Magic/plugins/__init__.py
@@ -1,12 +1,12 @@
 from glob import glob
 from os.path import basename, dirname, isfile
 
-def loadPlugins():
-    jalur = glob(f"{dirname(__file__)}/*.py")
+def sortir():
+    mod_paths = glob(f"{dirname(__file__)}/*.py")
     return sorted(
         [
-            basename(Magic)[:-3]
-            for Magic in jalur
-            if isfile(Magic) and Magic.endswith(".py") and not Magic.endswith("__init__.py")
+            basename(f)[:-3]
+            for f in mod_paths
+            if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
         ]
     )
