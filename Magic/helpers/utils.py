e3a9575a50905ed2d388b402f384ffa7c23f59a6
NotLumiere <118038685+NotLumiere@users.noreply.github.com>
2023-09-16 16:17:58 +0700

    Update utils.py

--- Magic/helpers/utils.py
+++ Magic/helpers/utils.py
@@ -1,1 +1,24 @@
+def get_arg(message: Message):
+sg = message.text
+    msg = msg.replace(" ", "", 1) if msg[1] == " " else msg
+    split = msg[1:].replace("\n", " \n").split(" ")
+    if " ".join(split[1:]).strip() == "":
+        return ""
+    return " ".join(split[1:])
 
+def get_args(message: Message):
+    try:
+        message = message.text
+    except AttributeError:
+        pass
+    if not message:
+        return False
+    message = message.split(maxsplit=1)
+    if len(message) <= 1:
+        return []
+    message = message[1]
+    try:
+        split = shlex.split(message)
+    except ValueError:
+        return message
+    return list(filter(lambda x: len(x) > 0, split))
