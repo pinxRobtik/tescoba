db621f7a2b25d47fa664fcecfe674d908e6a9ae1
NotLumiere <118038685+NotLumiere@users.noreply.github.com>
2023-09-16 16:17:10 +0700

    Update copy.py

--- Magic/plugins/copy.py
+++ Magic/plugins/copy.py
@@ -8,32 +8,9 @@ from pyrogram import Client, filters
 from pyrogram.errors import RPCError
 from pyrogram.types import *
 from Magic import *
+from Magic.helpers.utils import *
 from . import *
 
-def get_arg(message: Message):
-    msg = message.text
-    msg = msg.replace(" ", "", 1) if msg[1] == " " else msg
-    split = msg[1:].replace("\n", " \n").split(" ")
-    if " ".join(split[1:]).strip() == "":
-        return ""
-    return " ".join(split[1:])
-
-def get_args(message: Message):
-    try:
-        message = message.text
-    except AttributeError:
-        pass
-    if not message:
-        return False
-    message = message.split(maxsplit=1)
-    if len(message) <= 1:
-        return []
-    message = message[1]
-    try:
-        split = shlex.split(message)
-    except ValueError:
-        return message
-    return list(filter(lambda x: len(x) > 0, split))
 
 @ubot.on_message(filters.command("copy", ".") & filters.me)
 async def copy(client, message):
