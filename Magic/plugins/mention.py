2b6f50f2ccef12f46293417f26120bb1c0c300dc
NotLumiere <118038685+NotLumiere@users.noreply.github.com>
2023-09-16 16:21:35 +0700

    Update mention.py

--- Magic/plugins/mention.py
+++ Magic/plugins/mention.py
@@ -6,16 +6,6 @@ from config import *
 from Magic import *
 from Magic.helpers import *
 
-
-
-def get_arg(message: Message):
-    msg = message.text
-    msg = msg.replace(" ", "", 1) if msg[1] == " " else msg
-    split = msg[1:].replace("\n", " \n").split(" ")
-    if " ".join(split[1:]).strip() == "":
-        return ""
-    return " ".join(split[1:])
-
 spam_chats = []
 
 @ubot.on_message(filters.command("mention", prefix) & filters.me)
