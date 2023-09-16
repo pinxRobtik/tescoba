ce64f17c8d3457a3073e768cb9b9c8873d899e54
NotLumiere <118038685+NotLumiere@users.noreply.github.com>
2023-09-16 16:23:49 +0700

    Update utils.py

--- Magic/helpers/utils.py
+++ Magic/helpers/utils.py
@@ -1,5 +1,9 @@
+import os
+from pyrogram import Client, emoji, filters
+from pyrogram.types import Message
+
 def get_arg(message: Message):
-    sg = message.text
+    msg = message.text
     msg = msg.replace(" ", "", 1) if msg[1] == " " else msg
     split = msg[1:].replace("\n", " \n").split(" ")
     if " ".join(split[1:]).strip() == "":
